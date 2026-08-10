70f232a42c48b7be34ee0e523242b0ddaa693d98
magarjames <thapajames12@gmail.com>
2026-08-10 02:58:04 +0100

    fix: import useRef in NorthlineShop

--- src/components/northline/NorthlineShop.tsx
+++ src/components/northline/NorthlineShop.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import { shopifyClient } from "@/lib/shopify";
 import "./northline.css";
