a053a46f690061ad3cbd8620c5ec2e20dbc27c91
magarjames <thapajames12@gmail.com>
2026-08-13 12:55:50 +0100

    Revert "fix: make video extend down to the wave by removing padding"
    This reverts commit 4fff7bae25e27255d815674aa740c998cc816602.

--- src/components/northline/NorthlineFooter.tsx
+++ src/components/northline/NorthlineFooter.tsx
@@ -26,8 +26,8 @@ export function NorthlineFooter() {
       <div className="nl-footer-ripped-edge">
         <NorthlineScrollEdge 
           motion={motionRef} 
-          colour="#12131c" 
-          toneStrength={0.3} 
+          colour="#0f0f1c" 
+          toneStrength={0.1} 
           className="nl-footer-scroll-edge" 
         />
       </div>
